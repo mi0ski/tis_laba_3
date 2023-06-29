66b5db2a86c98152719f34075468b7bd628bb92a
mi0ski <137343999+mi0ski@users.noreply.github.com>
2023-06-29 11:34:55 +0300

    Add files via upload

---
+++ Mathematical operations with numbers in JavaScript/script8.js
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Моя первая программа</title>
+		<script>
+			alert('Привет, мир!');
+		let a = 1 + 2;
+		alert(a); // выведет 3
+		let b = 3 - 2;
+		alert(b); // выведет 1
+		let c = 3 * 2;
+		alert(c); // выведет 6
+		let d = 4 / 2;
+		alert(d); // выведет 2
+		</script>
+	</head>
+	<body>
+		моя первая программа
+	</body>
+</html>
