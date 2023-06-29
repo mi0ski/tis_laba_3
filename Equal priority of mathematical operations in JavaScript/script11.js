dc3abb248d45e590bbd97260732d71df9f6945c2
mi0ski <137343999+mi0ski@users.noreply.github.com>
2023-06-29 11:25:49 +0300

    Add files via upload

---
+++ Equal priority of mathematical operations in JavaScript/script11.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Моя первая программа</title>
+		<script>
+		let a = 8 / 2 * 4;
+		alert(a); // выведет 16 (результат 4 * 4)
+		let a = 8 * 2 / 4;
+		alert(a); // выведет 4 (результат 16 / 4)
+		let a = 16 / 2 / 2 / 2;
+		alert(a); // выведет 2
+		</script>
+	</head>
+	<body>
+		моя первая программа
+	</body>
+</html>
