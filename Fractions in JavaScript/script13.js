8ef1ae1d85611affbeb510902b5f1466b16eebd0
mi0ski <137343999+mi0ski@users.noreply.github.com>
2023-06-29 11:41:06 +0300

    Add files via upload

---
+++ Fractions in JavaScript/script13.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Моя первая программа</title>
+		<script>
+		let a = 1.5;
+		let b = 0.75;
+		let sum = a + b;
+		console.log(sum);
+		</script>
+	</head>
+	<body>
+		моя первая программа
+	</body>
+</html>
