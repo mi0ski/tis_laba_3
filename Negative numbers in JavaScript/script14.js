a5f8c32ed82d115e135c83a0872854d8afe918b9
mi0ski <137343999+mi0ski@users.noreply.github.com>
2023-06-29 11:39:37 +0300

    Add files via upload

---
+++ Negative numbers in JavaScript/script14.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Моя первая программа</title>
+		<script>
+		var a = -100;
+		console.log(a);
+		alert(a);
+		</script>
+	</head>
+	<body>
+		моя первая программа
+	</body>
+</html>
