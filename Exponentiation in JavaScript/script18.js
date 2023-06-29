d63c9dd2f86d0a5e6302d82d9adb8d4ffac87f8c
mi0ski <137343999+mi0ski@users.noreply.github.com>
2023-06-29 11:36:37 +0300

    Add files via upload

---
+++ Exponentiation in JavaScript/script18.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Моя первая программа</title>
+		<script>
+		let a = 2;
+		let b = 10;
+		alert(a ** b);
+		</script>
+	</head>
+	<body>
+		моя первая программа
+	</body>
+</html>
