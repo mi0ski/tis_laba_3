38a4ef8e0d539d5dfcc916b6aa007943abcb79e5
mi0ski <137343999+mi0ski@users.noreply.github.com>
2023-06-29 11:38:15 +0300

    Add files via upload

---
+++ Priority of mathematical operations in JavaScript/script10.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Моя первая программа</title>
+		<script>
+		let a = 2 * 2 + 3;
+		alert(a); // выведет 7 (результат 4 + 3)
+		</script>
+	</head>
+	<body>
+		моя первая программа
+	</body>
+</html>
